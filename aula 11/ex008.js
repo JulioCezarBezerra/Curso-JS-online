2eb2e0d81d490dc769201d463401f3935ec5825f
Júlio Bezerra <juliobezerradev@outlook.com>
2023-01-08 12:05:49 -0300

    commi

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`a velocidade do seu carro é ${vel} km por hora.`)
+if (vel > 60) {
+    console.log('vc passou do limite, multado!')
+}
+console.log('dirija com cuidado, use cinto de segurança.')
